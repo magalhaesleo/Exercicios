c0f6d16855ba5f0f3b26e39755ba10c930c51428
leonardo.magalhaes <leonardo.magalhaes@NDD-DESK-TES162>
2017-03-13 17:20:06 -0300

    Exercicio 3

---
+++ Exercicios/Exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o raio do cilindro: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a altura do cilindro: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double v = ((3.14 * (r * r)) * a);
+            Console.WriteLine("O volume do cilindro e: {0}",v);
+
+        }
+    }
+}
