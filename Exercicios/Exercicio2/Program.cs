3801c919f6c6443276a2215a3742fc6a3f0232a6
leonardo.magalhaes <leonardo.magalhaes@NDD-DESK-TES162>
2017-03-13 15:48:47 -0300

    Exercicio 2

--- Exercicios/Exercicio2/Program.cs
+++ Exercicios/Exercicio2/Program.cs
@@ -10,6 +10,11 @@ namespace Exercicio2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite a temperatura em Fahrenheit: ");
+            double tempFahrenheit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Temperatura em Fahrenheit e: {0}", tempFahrenheit);
+            double tempCelsius = (tempFahrenheit - 32) * (5.0/9.0);
+            Console.WriteLine("Temperatura em Celsius e: {0}", tempCelsius);
         }
     }
 }
