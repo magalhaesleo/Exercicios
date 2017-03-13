cdeabb668404ead5270d76ff3ebc9e6c89216b7c
leonardo.magalhaes <leonardo.magalhaes@NDD-DESK-TES162>
2017-03-13 14:53:49 -0300

    Exercicio 1

--- Exercicios/Exercicios/Program.cs
+++ Exercicios/Exercicios/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercicios
+namespace Exercicio1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string comprimento,largura,altura;
+            Console.WriteLine("==================Calculo de caixa retangular==================");
+            Console.WriteLine("Digite o comprimento da caixa:");
+            comprimento = Console.ReadLine();
+            Console.WriteLine("Digite a largura da caixa:");
+            largura = Console.ReadLine();
+            Console.WriteLine("Digite a altura da caixa:");
+            altura = Console.ReadLine();
+
+            int volume = Int32.Parse(comprimento) * Int32.Parse(largura) * Int32.Parse(altura);
+            Console.WriteLine("o volume da caixa é: {0}",volume);
         }
     }
 }
